ba5c33e1155810e237e7d98b14c4a7676f483afa
Luciano Ferrari <luciano.ferrari@mercadolibre.com>
2020-08-02 20:11:42 -0300

    item controller and service

---
+++ domain/items/item_dao.go
@@ -0,0 +1,11 @@
+package items
+
+import "github.com/ferralucho/store_items-api/utils/rest_errors"
+
+func (i *Item) Save() *rest_errors.RestErr {
+	return nil
+}
+
+func (i *Item) Get() *rest_errors.RestErr {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ferralucho/store_items-api
+
+go 1.12
+
+require (
+	github.com/ferralucho/store_oauth-go v0.0.0-20200802181939-bdf62bbafdd8
+	github.com/gorilla/mux v1.7.4
+)
